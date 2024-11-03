f4f36c25440e3de98d8dc1279cb7c6e39a2faf40
Pablo Rivera <p.riverah@uniandes.edu.co>
2024-11-02 22:58:12 -0500

    Se crea la clase de Coche

--- src/app/coches/coches.ts
+++ src/app/coches/coches.ts
@@ -1,2 +1,29 @@
 export class Coches {
+  id:string;
+  marca:string;
+  linea:string;
+  referencia:string;
+  modelo:string;
+  kilometraje:string;
+  color:string;
+  image:string;
+  constructor(cochesData:{
+    "id":string;
+    "marca":string;
+    "linea":string;
+    "referencia":string;
+    "modelo":string;
+    "kilometraje":string;
+    "color":string;
+    "image":string;
+  }){
+    this.id = cochesData.id;
+    this.marca = cochesData.marca;
+    this.linea = cochesData.linea;
+    this.referencia = cochesData.referencia;
+    this.modelo = cochesData.modelo;
+    this.kilometraje = cochesData.kilometraje;
+    this.color = cochesData.color;
+    this.image = cochesData.image;
+  }
 }
