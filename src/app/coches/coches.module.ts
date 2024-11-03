3f3c43c5be2cdbe3b5d01edb8f285c318fcdc646
Pablo Rivera <p.riverah@uniandes.edu.co>
2024-11-02 19:57:25 -0500

    Modulo y componentes de coches creados

---
+++ src/app/coches/coches.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CochesListComponent } from './coches-list/coches-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    CochesListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CochesModule { }
---
+++ src/app/coches/coches.spec.ts
@@ -0,0 +1,7 @@
+import { Coches } from './coches';
+
+describe('Coches', () => {
+  it('should create an instance', () => {
+    expect(new Coches()).toBeTruthy();
+  });
+});
